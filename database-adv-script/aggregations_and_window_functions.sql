847f4193682bbdd35ab2b6d1ba49282fcddfd383
Nahom Gebremichael <username@users.noreply.github.com>
2025-11-02 20:25:54 +0300

    fix:doesnt contain RANK()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,8 +12,15 @@ SELECT
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
-FROM properties p
-LEFT JOIN bookings b ON p.id = b.property_id
-GROUP BY p.id, p.name
-ORDER BY booking_rank;
+    ROW_NUMBER() OVER (
+        ORDER BY
+            COUNT(b.id) DESC
+    ) AS booking_rank
+FROM
+    properties p
+    LEFT JOIN bookings b ON p.id = b.property_id
+GROUP BY
+    p.id,
+    p.name
+ORDER BY
+    booking_rank;
